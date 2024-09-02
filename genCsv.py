e5eb344be48b1cb2bda62d89cfb9192bada06f0c
João Vitor Fröhlich <joaovitorfrohlich@gmail.com>
2024-09-02 15:06:47 -0300

    Scripts to create a CSV out of the JSONs

---
+++ genCsv.py
@@ -0,0 +1,56 @@
+import json
+import csv
+import os
+
+csv_data = [
+    ['File Name', 'Execution', 'Number of vertices', 'Number of edges', 'Min count', 'Max count', 'Hottest blocks', 'Random hottest guess', 'Random hit', 'Nested hottest guess', 'Nested hit']
+]
+
+gt_data = json.load(open('JSON Files/jotaiMerlinResults.json','r'))
+random_data = json.load(open('JSON Files/jotaiRandomBlock.json','r'))
+nested_data = json.load(open('JSON Files/jotaiNestedBlock.json','r'))
+
+benchmarks = os.listdir('Benchmark/Jotai')
+
+for app_name in gt_data:
+    for function_name in gt_data[app_name]:
+        file_name = 'extr_'+app_name+'.c_'+function_name+'.c'
+        if file_name not in benchmarks:
+            file_name = 'extr_'+app_name+'.h_'+function_name+'.c'
+        random_guess = random_data[app_name][function_name]
+        nested_guess = nested_data[app_name][function_name]
+        for execution_number in range(0,len(gt_data[app_name][function_name])):
+            graph = gt_data[app_name][function_name][execution_number]
+            frequencies = {}
+            nodes = set()
+            edges = 0
+            for u in graph:
+                nodes.add(u)
+                for v in graph[u]:
+                    edges += 1
+                    nodes.add(v)
+                    if v in frequencies:
+                        frequencies[v] += graph[u][v]
+                    else:
+                        frequencies[v] = graph[u][v]
+            nodes = len(nodes)
+            sorted_executions = sorted(frequencies.items(), key=lambda item: item[1])[::-1]
+            max_count = sorted_executions[0][1]
+            min_count = sorted_executions[-1][1]
+            hottest_blocks_list = []
+            for block, count in sorted_executions:
+                if count < max_count:
+                    break
+                hottest_blocks_list.append(block)
+            hottest_blocks = ';'.join(sorted(hottest_blocks_list, key=lambda x: int(x)))
+            random_hit = 0
+            if random_guess in hottest_blocks_list:
+                random_hit = 1
+            nested_hit = 0
+            if nested_guess in hottest_blocks_list:
+                nested_hit = 1
+            csv_data.append([file_name, execution_number, nodes, edges, min_count, max_count, str(hottest_blocks), random_guess, random_hit, nested_guess, nested_hit])
+
+with open('jotaiMerlinTable.csv', mode='w', newline='') as file:
+    writer = csv.writer(file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    writer.writerows(csv_data)
