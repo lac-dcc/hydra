c2d56e128f081d60f3750697f9b918dd60c94bfb
João Vitor Fröhlich <joao.frohlich@dcc.ufmg.br>
2025-03-20 14:13:43 -0300

    Adding cBench cross experiments

---
+++ Cross Experiments/cBench/iv-licm/runExperiment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Experiment for profile projection
+# From:
+#   O0 + mem2reg, instnamer, break-crit-edges, loop-simplify
+# To:
+#   O0 + mem2reg, instnamer, break-crit-edges, loop-simplify
+
+export EXP_FOLDER="iv-licm"
+export PASSES="indvars licm"
+
+export LLVM_INSTALL_DIR="/usr/local"
+export BASE_DIR="/home/jvf/Codes/hydra"
+CUR_DIR="$BASE_DIR/Cross Experiments/cBench/$EXP_FOLDER"
+
+echo "Running nisse_all script"
+bash "$CUR_DIR/cBench/nisse_all.sh"
+
+echo "Running hot block prediction heuristics"
+bash "$CUR_DIR/runHotBlock.sh"
+
+echo "Running block ordering prediction heuristics"
+bash "$CUR_DIR/runBlockOrdering.sh"
+
+echo "Generating CSV files"
+python3 "$CUR_DIR/genHBPCsv.py"
+python3 "$CUR_DIR/genBOPCsv.py"
